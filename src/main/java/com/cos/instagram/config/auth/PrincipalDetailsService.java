60e43767b28885dc5a53e0d2d784519776392e3e
hyeonwook <hyeonwook98@naver.com>
2023-03-20 00:32:07 +0900

    feat: 스프링 시큐리티 - 로그인 구현

--- src/main/java/com/cos/instagram/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/instagram/config/auth/PrincipalDetailsService.java
@@ -1,15 +1,28 @@
 package com.cos.instagram.config.auth;
 
+import com.cos.instagram.domain.user.User;
+import com.cos.instagram.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class PrincipalDetailsService implements UserDetailsService {
 
-    @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    private final UserRepository userRepository;
+
+    @Override //비밀번호는 시큐리티가 알아서 처리해준다.
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsername(username);
+
+        if (userEntity == null) {
+            return null;
+        } else {
+            return new PrincipalDetails(userEntity);
+        }
+
     }
 }
--- src/main/java/com/cos/instagram/domain/user/UserRepository.java
+++ src/main/java/com/cos/instagram/domain/user/UserRepository.java
@@ -3,4 +3,5 @@ package com.cos.instagram.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUsername(String username);
 }
