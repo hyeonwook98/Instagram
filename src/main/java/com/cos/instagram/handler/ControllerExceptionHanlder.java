8ac136a4cb95c332ddecdf2c10fa617945fd8905
hyeonwook <hyeonwook98@naver.com>
2023-03-19 23:06:04 +0900

    feat: 공통 응답 DTO 만들기

--- src/main/java/com/cos/instagram/handler/ControllerExceptionHanlder.java
+++ src/main/java/com/cos/instagram/handler/ControllerExceptionHanlder.java
@@ -1,18 +1,20 @@
 package com.cos.instagram.handler;
 
 import com.cos.instagram.handler.ex.CustomValidationException;
+import com.cos.instagram.web.dto.CMRespDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+
 @RestController
 @ControllerAdvice
 public class ControllerExceptionHanlder {
 
     @ExceptionHandler(CustomValidationException.class)
-    public Map<String, String> validationException(CustomValidationException e) {
-        return e.getErrorMap();
+    public CMRespDto<?> validationException(CustomValidationException e) {
+        return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrorMap());
     }
 }
---
+++ src/main/java/com/cos/instagram/web/dto/CMRespDto.java
@@ -0,0 +1,16 @@
+package com.cos.instagram.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CMRespDto<T> {
+
+    private int code;
+    private String message;
+    private T data;
+}
