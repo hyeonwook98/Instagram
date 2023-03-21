6f50c735fc317b359818c5fbfc11134024418abd
hyeonwook <hyeonwook98@naver.com>
2023-03-21 23:10:24 +0900

    feat: 팔로우모달 정보에서 팔로우 및 팔로우취소 구현

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -41,7 +41,7 @@ function toggleSubscribe(toUserId, obj) {
 	}
 }
 
-// (2) 구독자 정보  모달 보기
+// (2) 구독자 정보 모달 보기
 function subscribeInfoModalOpen(pageUserId) {
 	$(".modal-subscribe").css("display", "flex");
 
@@ -70,30 +70,18 @@ function getSubscribeModalItem() {
 
 	if (!u.equalUserState) {
 		if (u.subscribeState) {
-			item+=`<button class="cta blue" onclick="toggleSubscribeModal(this)">구독취소</button>`;
+			item+=`<button class="cta blue" onclick="toggleSubscribe(${u.id},this)">구독취소</button>`;
 		} else {
-			item+=`<button class="cta" onclick="toggleSubscribeModal(this)">구독하기</button>`;
+			item+=`<button class="cta" onclick="toggleSubscribe(${u.id},this)">구독하기</button>`;
 		}
 	}
-
 	item += `
     </div>
 </div>`
 }
 
 
-// (3) 구독자 정보 모달에서 구독하기, 구독취소
-function toggleSubscribeModal(obj) {
-	if ($(obj).text() === "구독취소") {
-		$(obj).text("구독하기");
-		$(obj).toggleClass("blue");
-	} else {
-		$(obj).text("구독취소");
-		$(obj).toggleClass("blue");
-	}
-}
-
-// (4) 유저 프로파일 사진 변경 (완)
+// (3) 유저 프로파일 사진 변경 (완)
 function profileImageUpload() {
 	$("#userProfileImageInput").click();
 
@@ -115,7 +103,7 @@ function profileImageUpload() {
 }
 
 
-// (5) 사용자 정보 메뉴 열기 닫기
+// (4) 사용자 정보 메뉴 열기 닫기
 function popup(obj) {
 	$(obj).css("display", "flex");
 }
@@ -125,17 +113,17 @@ function closePopup(obj) {
 }
 
 
-// (6) 사용자 정보(회원정보, 로그아웃, 닫기) 모달
+// (5) 사용자 정보(회원정보, 로그아웃, 닫기) 모달
 function modalInfo() {
 	$(".modal-info").css("display", "none");
 }
 
-// (7) 사용자 프로파일 이미지 메뉴(사진업로드, 취소) 모달
+// (6) 사용자 프로파일 이미지 메뉴(사진업로드, 취소) 모달
 function modalImage() {
 	$(".modal-image").css("display", "none");
 }
 
-// (8) 구독자 정보 모달 닫기
+// (7) 구독자 정보 모달 닫기
 function modalClose() {
 	$(".modal-subscribe").css("display", "none");
 	location.reload();
