dca3621994e5de1808b478a9bc6147567564e4d1
hyeonwook <hyeonwook98@naver.com>
2023-03-22 18:54:11 +0900

    feat: 이미지리스트 페이징 처리

--- src/main/java/com/cos/instagram/domain/image/ImageRepository.java
+++ src/main/java/com/cos/instagram/domain/image/ImageRepository.java
@@ -1,11 +1,14 @@
 package com.cos.instagram.domain.image;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Integer> {
-    @Query(value = "SELECT * FROM image WHERE userId IN (SELECT toUserId FROM subscribe where fromUserId = :principalId);", nativeQuery = true)
-    List<Image> mStory(int principalId);
+    @Query(value = "SELECT * FROM image WHERE userId IN (SELECT toUserId FROM subscribe where fromUserId = :principalId) " +
+            "ORDER BY id DESC", nativeQuery = true)
+    Page<Image> mStory(int principalId, Pageable pageable);
 }
--- src/main/java/com/cos/instagram/service/ImageService.java
+++ src/main/java/com/cos/instagram/service/ImageService.java
@@ -6,6 +6,8 @@ import com.cos.instagram.domain.image.ImageRepository;
 import com.cos.instagram.web.dto.image.ImageUploadDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import org.springframework.transaction.annotation.Transactional;
@@ -24,8 +26,8 @@ public class ImageService {
     private String uploadFolder;
 
     @Transactional(readOnly = true)
-    public List<Image> 이미지스토리(int principalId) {
-        List<Image> images = imageRepository.mStory(principalId);
+    public Page<Image> 이미지스토리(int principalId, Pageable pageable) {
+        Page<Image> images = imageRepository.mStory(principalId, pageable);
         return images;
     }
 
