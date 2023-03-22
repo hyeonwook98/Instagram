bd12f6d25f699686f84b6ce595f9fe8f697cbc61
hyeonwook <hyeonwook98@naver.com>
2023-03-23 02:06:08 +0900

    feat: 프로필 페이지 유저 사진 변경

--- src/main/java/com/cos/instagram/service/UserService.java
+++ src/main/java/com/cos/instagram/service/UserService.java
@@ -3,14 +3,22 @@ package com.cos.instagram.service;
 import com.cos.instagram.domain.subscribe.SubscribeRepository;
 import com.cos.instagram.domain.user.User;
 import com.cos.instagram.domain.user.UserRepository;
+import com.cos.instagram.handler.ex.CustomApiException;
 import com.cos.instagram.handler.ex.CustomException;
 import com.cos.instagram.handler.ex.CustomValidationApiException;
 import com.cos.instagram.handler.ex.CustomValidationException;
 import com.cos.instagram.web.dto.user.UserProfileDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
@@ -19,6 +27,29 @@ public class UserService {
     private final UserRepository userRepository;
     private final SubscribeRepository subscribeRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Value("${file.path}")
+    private String uploadFolder;
+
+    @Transactional
+    public User 회원프로필사진변경(int principalId, MultipartFile profileImageFile) {
+        UUID uuid = UUID.randomUUID();
+        String imageFileName = uuid + "_" +profileImageFile.getOriginalFilename();
+
+        Path imageFilePath = Paths.get(uploadFolder + imageFileName);
+
+        try {
+            Files.write(imageFilePath, profileImageFile.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        User userEntity = userRepository.findById(principalId).orElseThrow(() -> {
+            throw new CustomApiException("유저를 찾을 수 없습니다.");
+        });
+        userEntity.setProfileImageUrl(imageFileName);
+        return userEntity;
+    }
 
     @Transactional(readOnly=true)
     public UserProfileDto 회원프로필(int pageUserId, int principalId) {
