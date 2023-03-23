0fe561288b0012a203f5eeb23f7b896c55456512
hyeonwook <hyeonwook98@naver.com>
2023-03-23 12:01:54 +0900

    feat: Comment 엔티티 설계

---
+++ src/main/java/com/cos/instagram/domain/comment/CommentRepository.java
@@ -0,0 +1,6 @@
+package com.cos.instagram.domain.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+}
