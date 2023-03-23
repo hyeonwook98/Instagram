5c474c5adb4c5b163341149617dca5e4cd30b527
hyeonwook <hyeonwook98@naver.com>
2023-03-23 19:31:38 +0900

    feat: OAuth2 페이스북 로그인 - 회원정보 받기

--- src/main/java/com/cos/instagram/config/SecurityConfig.java
+++ src/main/java/com/cos/instagram/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.cos.instagram.config;
 
+import com.cos.instagram.config.oauth.OAuth2DetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
 
+    private final OAuth2DetailsService oAuth2DetailsService;
     @Bean
     BCryptPasswordEncoder encode() {
         return new BCryptPasswordEncoder();
@@ -23,9 +27,13 @@ public class SecurityConfig {
                 .permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/auth/signin")
-                .loginProcessingUrl("/auth/signin")
-                .defaultSuccessUrl("/");
+                .loginPage("/auth/signin") //GET
+                .loginProcessingUrl("/auth/signin") //POST -> 스프링 시큐리티가 로그인 프로세스 진행
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login() //oauth2 로그인도 할거다.
+                .userInfoEndpoint() //인증코드나 token 권한 대행은 알아서 해주고 최종응답을 회원정보만 달라.
+                .userService(oAuth2DetailsService);
         return http.build();
     }
 }
